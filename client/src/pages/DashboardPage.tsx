0e916fd2b3593d369153cecdbd938de3d14f68eb
itsYuuuka <okan_altun@hotmail.com>
2026-04-30 19:53:31 +0200

    prevent body overflow reset

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -53,12 +53,12 @@ export default function DashboardPage() {
       document.body.style.overflow = "hidden";
       document.documentElement.style.overflow = "hidden";
     } else {
-      document.body.style.overflow = "";
-      document.documentElement.style.overflow = "";
+      document.body.style.overflow = "hidden";
+      document.documentElement.style.overflow = "hidden";
     }
     return () => {
-      document.body.style.overflow = "";
-      document.documentElement.style.overflow = "";
+      document.body.style.overflow = "hidden";
+      document.documentElement.style.overflow = "hidden";
     };
   }, [selectedTemplate]);
 
